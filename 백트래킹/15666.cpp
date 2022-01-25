a069383a07edf0a279b0a26671b7d5878b1d4a4d
Soyun Won <88621012+1sxyzn@users.noreply.github.com>
2022-01-25 20:27:13 +0900

    [백트래킹] N과 M (12)

---
+++ 백트래킹/15666.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+//N과 M (8) 에서 중복되는 수 제거하는 코드 한 줄 추가
+
+const int SIZE=8;
+vector<int> num; // n의 수를 입력 받을 벡터
+int number[SIZE]; //답을 저장할 배열
+bool check[10001]; //방문 확인할 벡터
+int n,m;
+
+void backtracking(int index, int start){
+    //종료조건
+    if(index==m) {
+        for (int i = 0; i < index; i++)
+            cout << number[i] << ' ';
+        cout<<'\n';
+        return;
+    }
+    //백트래킹
+    for(int i=start; i<num.size(); i++) {
+        number[index]=num[i]; //수열에 값 저장
+        backtracking(index+1, i); //다음 수 검사
+    }
+}
+
+int main() {
+    cin>>n>>m;
+    int temp = n;
+    int i;
+    while(temp--){
+        cin>>i;
+        num.push_back(i);
+    }
+    sort(num.begin(), num.end()); //오름차순 정렬
+    num.erase(unique(num.begin(),num.end()),num.end()); //(추가 코드) 중복되는 수 제거
+
+    backtracking(0, 0);
+}
