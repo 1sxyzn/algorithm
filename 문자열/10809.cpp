53db768568ba20dc33d6219532d87859a44e7d34
Soyun Won <88621012+1sxyzn@users.noreply.github.com>
2022-10-09 00:38:07 +0900

    [문자열] 알파벳 찾기

---
+++ 문자열/10809.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    vector<int>v(26,-1);
+    for(int i=0; i<s.size(); i++){
+        if(v[s[i]-'a']==-1) v[s[i]-'a']=i;
+    }
+    for(int i=0; i<26; i++){
+        cout<<v[i]<<' ';
+    }
+}
