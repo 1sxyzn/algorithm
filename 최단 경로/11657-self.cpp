909a824657a6228937326925e0d6c846887452ed
Soyun Won <88621012+1sxyzn@users.noreply.github.com>
2023-06-18 00:08:31 +0900

    [벨만-포드] 타임머신 - 에러 수정

--- 최단 경로/11657-self.cpp
+++ 최단 경로/11657-self.cpp
@@ -19,7 +19,7 @@ int main(){
     cin>>n>>m;
 
     vector<long long> dist(n+1,LONG_LONG_MAX);
-    vector<tuple<int,int,int>> graph(n+1);
+    vector<tuple<int,int,int>> graph;
 
     int a,b,c;
     for(int i=0; i<m; i++){
@@ -32,7 +32,7 @@ int main(){
     // 벨만 포드
     int check = n-1;
     int s,e,w;
-    // 사이클 없으면 n번 이상을 돌릴 때, 값이 그대로인데 사이클 있으면 계속 값이 변함. 그래서 일단 n-1번 돌리고 한 번 더 돌리면서 값이 변하나 보자...
+    // n번 이상을 돌릴 때, 사이클 없으면 값이 그대로인데 사이클 있으면 계속 값이 변함. 그래서 일단 n-1번 돌리고 한 번 더 돌리면서 값이 변하나 보자...
     // ( -> 노드가 n개면 최대 n-1개의 간선이 생김)
     while(check--){
         for(int i=0; i<m; i++){
@@ -47,7 +47,7 @@ int main(){
     // 음수 사이클 판별
     bool cycle = false;
 
-    for(int i=0; i<m; i++){ // n번째 반복
+    for(int i=0; i<m; i++){ // n번째 반복 -> 여기서 값이 변하면 cycle이 생겼다는 뜻임
         s = get<0>(graph[i]);
         e = get<1>(graph[i]);
         w = get<2>(graph[i]);
