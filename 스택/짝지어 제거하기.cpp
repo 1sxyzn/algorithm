f8cfa5c3b6c8ea1a08167533e919185457c184cf
Soyun Won <88621012+1sxyzn@users.noreply.github.com>
2023-09-21 13:02:42 +0900

    [스택] 짝지어 제거하기 - 프로그래머스

---
+++ 스택/짝지어 제거하기.cpp
@@ -0,0 +1,16 @@
+#include<string>
+#include<stack>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = -1;
+    stack<char>st;
+    for(int i=0; i<s.length(); i++){
+        if(st.empty()||st.top()!=s[i]) st.push(s[i]);
+        else st.pop();
+    }
+    if(st.empty()) answer = 1;
+    else answer = 0;
+    return answer;
+}
